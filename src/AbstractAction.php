9372a740a34a70a019e3c59cfa9d3573ec5dd9eb
David Dashifen Kees <dashifen@dashifen.com>
2017-05-07 11:16:59 -0400

    added Container to execute for access to services

--- src/AbstractAction.php
+++ src/AbstractAction.php
@@ -5,6 +5,7 @@ namespace Dashifen\Action;
 use Dashifen\Domain\DomainInterface;
 use Dashifen\Request\RequestInterface;
 use Dashifen\Response\ResponseInterface;
+use Interop\Container\ContainerInterface;
 
 abstract class AbstractAction implements ActionInterface {
 	/**
@@ -41,8 +42,16 @@ abstract class AbstractAction implements ActionInterface {
 	
 	/**
 	 * @param string $parameter
+	 * @param ContainerInterface $container
+	 *
+	 * the purpose of passing the $container into the execute method is so
+	 * that the method can access any services that have been defined for the
+	 * application.  e.g., some actions might need to send an email, but we
+	 * don't want to add that behavior to all actions.  instead, we'll just
+	 * provide the $container so that an action can request access to a
+	 * predefined service therein.
 	 *
 	 * @return ResponseInterface
 	 */
-	abstract public function execute(string $parameter = ""): ResponseInterface;
+	abstract public function execute(string $parameter = "", ContainerInterface $container = null): ResponseInterface;
 }
