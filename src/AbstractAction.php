b225b5124d34829a3ae64e48082744116fa4cf03
David Dashifen Kees <dashifen@dashifen.com>
2019-07-04 14:30:51 -0400

    added psr-11 interface

--- src/AbstractAction.php
+++ src/AbstractAction.php
@@ -2,56 +2,56 @@
 
 namespace Dashifen\Action;
 
-use Aura\Di\Container;
 use Dashifen\Domain\DomainInterface;
+use Psr\Container\ContainerInterface;
 use Dashifen\Request\RequestInterface;
 use Dashifen\Response\ResponseInterface;
 
 abstract class AbstractAction implements ActionInterface {
-	/**
-	 * @var RequestInterface $request
-	 */
-	protected $request;
-	
-	/**
-	 * @var DomainInterface $domain
-	 */
-	protected $domain;
-	
-	/**
-	 * @var ResponseInterface $response
-	 */
-	protected $response;
-	
-	/**
-	 * @var Container $container
-	 */
-	protected $container;
-	
-	/**
-	 * AbstractAction constructor.
-	 *
-	 * @param RequestInterface   $request
-	 * @param DomainInterface    $domain
-	 * @param ResponseInterface  $response
-	 * @param Container $container
-	 */
-	public function __construct(
-		RequestInterface $request,
-		DomainInterface $domain,
-		ResponseInterface $response,
-		Container $container
-	) {
-		$this->request = $request;
-		$this->domain = $domain;
-		$this->response = $response;
-		$this->container = $container;
-	}
-	
-	/**
-	 * @param array $parameter
-	 *
-	 * @return ResponseInterface
-	 */
-	abstract public function execute(array $parameter = []): ResponseInterface;
+  /**
+   * @var RequestInterface $request
+   */
+  protected $request;
+
+  /**
+   * @var DomainInterface $domain
+   */
+  protected $domain;
+
+  /**
+   * @var ResponseInterface $response
+   */
+  protected $response;
+
+  /**
+   * @var ContainerInterface $container
+   */
+  protected $container;
+
+  /**
+   * AbstractAction constructor.
+   *
+   * @param RequestInterface   $request
+   * @param DomainInterface    $domain
+   * @param ResponseInterface  $response
+   * @param ContainerInterface $container
+   */
+  public function __construct (
+    RequestInterface $request,
+    DomainInterface $domain,
+    ResponseInterface $response,
+    ContainerInterface $container
+  ) {
+    $this->request = $request;
+    $this->domain = $domain;
+    $this->response = $response;
+    $this->container = $container;
+  }
+
+  /**
+   * @param array $parameter
+   *
+   * @return ResponseInterface
+   */
+  abstract public function execute (array $parameter = []): ResponseInterface;
 }
